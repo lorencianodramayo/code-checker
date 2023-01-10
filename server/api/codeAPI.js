32e721d20eae506afc9ebc9c3f749b9eae8eb950
Lorenciano Dramayo <lorencianodramayo@gmail.com>
2023-01-10 15:27:39 +0800

    fixed some sheesh on code checker API

--- server/api/codeAPI.js
+++ server/api/codeAPI.js
@@ -2,29 +2,33 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.post("/", (req, res) => {
-  axios
-    .post(
+router.post("/", async (req, res) => {
+  try {
+    const response = await axios.post(
       "https://api.diffchecker.com/public/text?output_type=html&email=lorencianodramayo@gmail.com",
       req.body
-    )
-    .then((response) =>
-      axios
-        .post(
-          "https://api.diffchecker.com/public/text?output_type=json&email=lorencianodramayo@gmail.com",
-          req.body
-        )
-        .then((resJson) =>
-          res.status(200).json({
-            code: response?.data,
-            details: {
-              lines: resJson.data?.rows,
-              added: resJson?.data?.added,
-              removed: resJson?.data?.removed,
-            },
-          })
-        )
     );
+
+    try {
+      const resJson = await axios.post(
+        "https://api.diffchecker.com/public/text?output_type=json&email=lorencianodramayo@gmail.com",
+        req.body
+      );
+
+      res.status(200).json({
+        code: response?.data,
+        details: {
+          lines: resJson.data?.rows,
+          added: resJson?.data?.added,
+          removed: resJson?.data?.removed,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
