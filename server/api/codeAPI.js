d7557cf32eefc8cd4299da4c0ab27d898e12cb69
Lorenciano Dramayo <lorenciano.dramayo@smartly.io>
2023-04-14 23:21:53 +0800

    updated credentials

--- server/api/codeAPI.js
+++ server/api/codeAPI.js
@@ -6,13 +6,13 @@ router.post("/", async (req, res) => {
   console.log(req.body);
   try {
     const response = await axios.post(
-      "https://api.diffchecker.com/public/text?output_type=html&email=cianodev@gmail.com",
+      "https://api.diffchecker.com/public/text?output_type=html&email=hazel.dimaano@smartly.io",
       req.body
     );
 
     try {
       const resJson = await axios.post(
-        "https://api.diffchecker.com/public/text?output_type=json&email=cianodev@gmail.com",
+        "https://api.diffchecker.com/public/text?output_type=json&email=hazel.dimaano@smartly.io",
         req.body
       );
 
