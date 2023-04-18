8762eb1b852b4e53d5b69ab1f5de4bb46432cfe7
Lorenciano Dramayo <lorenciano.dramayo@smartly.io>
2023-04-18 18:08:01 +0800

    updated header for key

--- server/api/codeAPI.js
+++ server/api/codeAPI.js
@@ -7,13 +7,23 @@ router.post("/", async (req, res) => {
   try {
     const response = await axios.post(
       "https://api.diffchecker.com/public/text?output_type=html&email=hazel.dimaano@smartly.io",
-      req.body
+      req.body,
+      {
+        headers: {
+          "X-Api-Key": "116e879e-a413-4ccf-b505-e1cf0ccd034e",
+        },
+      }
     );
 
     try {
       const resJson = await axios.post(
         "https://api.diffchecker.com/public/text?output_type=json&email=hazel.dimaano@smartly.io",
-        req.body
+        req.body,
+        {
+          headers: {
+            "X-Api-Key": "116e879e-a413-4ccf-b505-e1cf0ccd034e",
+          },
+        }
       );
 
       res.status(200).json({
